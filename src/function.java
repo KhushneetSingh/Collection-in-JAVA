6c6b486c6da25e87c3218512176f51f5a0b25cc6
Khushneet Singh <singhkhushneet601@gmail.com>
2025-07-25 00:13:10 +0530

    passing the parameters and checking return

--- src/function.java
+++ src/function.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 public class function {
     public static void main(String[] args) {
-        sum();
+        int ans = sum();  // Call the sum function
+        System.out.println("The sum is: " + ans);
+        // sub(45, 45);  // Call the sub function, but we don't do anything with the result
+        System.out.println("The answer from sb finction : "+sub(34,45));
     }
-    static void sum(){
+    static int sum(){   //Can't be void, must return an int
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers to sum:");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int result = a + b;
-        System.out.println("The sum is: " + result);
+        // System.out.println("The sum is: " + result);
+        return result;  // Return the result instead of printing it and it will end the function
+
+        // System.out.println("This line will never be executed");  // This line is unreachable
+    }
+    static int sub(int a, int b) {
+        return a - b;  // Example of another function that could be added
     }
 }
