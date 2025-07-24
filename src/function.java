a3c839fd3f6f9f4187752a74e0e2307a73060496
Khushneet Singh <singhkhushneet601@gmail.com>
2025-07-25 00:20:40 +0530

    swapping value in func/method

--- src/function.java
+++ src/function.java
@@ -1,24 +1,32 @@
 import java.util.*;
 
+// This class demonstrates passing strings to methods and how Java handles string immutability.
 public class function {
     public static void main(String[] args) {
-        int ans = sum();  // Call the sum function
-        System.out.println("The sum is: " + ans);
-        // sub(45, 45);  // Call the sub function, but we don't do anything with the result
-        System.out.println("The answer from sb finction : "+sub(34,45));
-    }
-    static int sum(){   //Can't be void, must return an int
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two numbers to sum:");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int result = a + b;
-        // System.out.println("The sum is: " + result);
-        return result;  // Return the result instead of printing it and it will end the function
+        // Declare and initialize a string variable
+        String chacha = "Iron man";
+        // Call greet method with chacha
+        greet(chacha);
+        // Print the value of chacha after method call
+        System.out.println("chacha = " + chacha);
 
-        // System.out.println("This line will never be executed");  // This line is unreachable
+        // Declare and initialize another string variable
+        String mae = "Khushneet";
+        // Call great method with mae
+        great(mae);
+        // Print the value of mae after method call
+        System.out.println("mae = " + mae);
     }
-    static int sub(int a, int b) {
-        return a - b;  // Example of another function that could be added
+
+    // Method to greet a person by name
+    static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    // Method to demonstrate that changing the parameter does not affect the original variable
+    static void great(String naam){
+        // Assign a new value to the parameter
+        naam = "Singh";
+        System.out.println("Hello, " + naam + "!");
     }
 }
