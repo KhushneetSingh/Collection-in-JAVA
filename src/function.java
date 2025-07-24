dd3bcfe08dec3be5ad99969c8fc50431a82dcb7a
Khushneet Singh <singhkhushneet601@gmail.com>
2025-07-24 23:32:55 +0530

    getting hands on the code

--- src/function.java
+++ src/function.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class function {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter number 2: ");
+        int num2 = sc.nextInt();
+        
+        int sum2 = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum2);
     }
 }
