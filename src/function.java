5e39fccba165f27ca673d99886b40941035ec21a
Khushneet Singh <singhkhushneet601@gmail.com>
2025-07-24 23:47:01 +0530

    simple function

--- src/function.java
+++ src/function.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 public class function {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        sum();
+    }
+    static void sum(){
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter number 1: ");
-        int num1 = sc.nextInt();
-
-        System.out.println("Enter number 2: ");
-        int num2 = sc.nextInt();
-        
-        int sum2 = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum2);
+        System.out.println("Enter two numbers to sum:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a + b;
+        System.out.println("The sum is: " + result);
     }
 }
