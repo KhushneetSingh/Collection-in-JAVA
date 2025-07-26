8799d6523707a45a6737cb51cd6908048b9ccdf6
Khushneet Singh <singhkhushneet601@gmail.com>
2025-07-26 23:44:58 +0530

    constructor overloading and this keyword importance and use

--- src/OOPs/classobj.java
+++ src/OOPs/classobj.java
@@ -8,15 +8,21 @@ public class classobj {
 
         System.out.println("Name: " + p1.name+ ", Age: " + p1.age);
 
-        Person p2 = new Person();   // Default constructor creates another instance
+        Person p2 = new Person(22, "Khushneet Singh Sardar");   // Default constructor creates another instance
         p2.name = "Jane";
         p2.age = 25;
 
         System.out.println("Name: " + p2.name + ", Age: " + p2.age);
 
+        Person p3 = new Person("Singh", 25);
+        p3.name = "Singh";
+        p3.age = 25;
+
         p1.eat();  // Call the eat method on p1
         p2.walk(); // Call the walk method on p2
         p2.walk(2); // Call the overloaded walk method on p2
+        p3.walk(5); // Call the overloaded walk method on p3
+        System.out.println("Total persons created: " + Person.count); // Access static variable count
     }
 }
 
@@ -25,9 +31,22 @@ class Person {
     int age;    // Property for age
 
     static int count = 0; // Static variable means it is property of the class, not instance do not need to create object to access it
-    Person() {   // Default constructor
+    public Person() {   // Default constructor
         count++;
         System.out.println("A new person has been created.");
+    }
+
+    public Person(int newAge, String newName){
+        this(); // Call the constructor that is of the same class
+        name = newName; // Constructor with parameters to initialize name and age
+        age = newAge;
+    }
+
+    public Person(String name, int age) {
+        this.name = name; // Use 'this' to refer to the instance variable
+        this.age = age;   // Initialize the instance variables
+        count++; // Increment the static count variable
+        System.out.println("A new person has been created with name: " + name + " and age: " + age);
     }
 
     void walk() {   // Behavior method for walking
